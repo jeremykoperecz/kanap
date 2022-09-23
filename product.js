4a1697a2d9a143cd1b27cde43d4b365a2ddcec51
jeremykoperecz <kopereczjeremy@gmail.com>
2022-09-23 13:07:12 +0200

    product page

---
+++ product.js
@@ -0,0 +1,31 @@
+const product = window.location.search.split("?").join("");
+
+let productData = [];
+
+const fetchProduct = async () => {
+    await fetch(`http://localhost:3000/api/products/${product}`)
+        .then((res) => res.json())
+        .then((data) => { 
+            productData = data
+            console.log(productData);
+        });
+};
+
+const productDisplay = async () => {
+    await fetchProduct();
+    const containerImg = document.querySelector(".item__img");
+
+
+    containerImg.innerHTML = `
+    <img class=" item__img__img" src="${productData.imageUrl}" alt="image de canap ${productData.altTxt}"/>`
+
+    document.getElementById("title").innerHTML = `${productData.name}`;
+    document.getElementById("price").innerHTML = `${productData.price}`;
+    document.getElementById("description").innerHTML = `${productData.description}`;
+
+};
+ 
+    
+
+
+productDisplay();
---
+++ script.js
@@ -0,0 +1,41 @@
+let canapData = [];
+/*preparation de l'api*/
+const fetchCanap = async () => {
+    await fetch("http://localhost:3000/api/products")
+        .then((res) => res.json())
+        .then((data) => {
+            canapData = data;
+            console.log(canapData);
+        });
+};
+/*creation de la page index*/
+const canapDisplay = async () => {
+    await fetchCanap();
+
+    document.getElementById("items").innerHTML = canapData.map((canap) => `
+    <div class="items items_a">
+    <div id="${canap._id}" class="items_article">
+    <h3 class="items_article_h3">${canap.name}</h3>
+    <img class="items_article_img" src="${canap.imageUrl}" alt="image de canap ${canap.altTxt}"/>
+    <p class="items_article_p ">${canap.description}</p>
+    </div>
+    </div>
+    `)
+    /*supression des virgules*/
+        .join("");
+    /*envoie de l'url a la page product*/
+    let idCart = document.querySelectorAll(".items_article");
+    
+    
+    
+    idCart.forEach((items_article) => 
+        items_article.addEventListener("click", () => {
+            console.log(items_article);
+
+         window.location = `product.html?${items_article.id}`;
+    
+       }), 
+     );
+    
+};
+        canapDisplay();
