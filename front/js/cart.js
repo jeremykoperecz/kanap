28c71ad33e937ed20e31cd390c330fc49934222f
jeremykoperecz <kopereczjeremy@gmail.com>
2022-10-06 15:06:53 +0200

    page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -13,6 +13,8 @@ function retrieve() {
         const itemObject = JSON.parse(item);
         cart.push(itemObject);
     }
+ 
+
 }  
 function displayItem(item) {
     const article = makeArticle(item)
@@ -22,26 +24,62 @@ function displayItem(item) {
     article.appendChild(div)
     const cartItemContent = makeCartItemContent(item)
     article.appendChild(cartItemContent)
+    displayTotalQuantity(item)
+
 }
 function makeCartItemContent(item) {
-    const div = document.createElement('div')
-    div.classList.add('cart__item__content')
+    const divMakeCart = document.createElement('div')
+    divMakeCart.classList.add('cart__item__content')
+
     const description = document.createElement('div') 
-    description.classList.add('cart__item__description')
+    description.classList.add('cart__item__content__description')
+
     const h2 = document.createElement('h2')
     h2.textContent = item.name
     const p = document.createElement('p')
     p.textContent = item.color
     const p2 = document.createElement('p')
     p2.textContent = item.price + "€"
+
     description.appendChild(h2)
     description.appendChild(p)
     description.appendChild(p2)
-    div.appendChild(description)
-    return div
+    divMakeCart.appendChild(description)
+    
+    const settings = document.createElement('div');
+    settings.classList.add('cart__item__content__settings');
+    divMakeCart.appendChild(settings);
 
+    const itemQuantity = document.createElement('div');
+    itemQuantity.classList.add('cart__item__content__settings__quantity');
 
+    const pQuantity = document.createElement('p');
+    pQuantity.textContent = ('Qté : ')
+    itemQuantity.appendChild(pQuantity)
+
+    const buttonQuantity = document.createElement('input');
+    buttonQuantity.type = 'number';
+    buttonQuantity.classList.add('itemQuantity');
+    buttonQuantity.name = 'itemQuantity';
+    buttonQuantity.value = item.quantity;
+    buttonQuantity.min = '1';
+    buttonQuantity.max = '100';
+
+    const deleteItem = document.createElement('div');
+    deleteItem.classList.add('cart__item__content__settings__delete');
+    const pDelete = document.createElement('p');
+    pDelete.classList.add('deleteItem');
+    pDelete.textContent = 'Supprimer';
+    
+
+    itemQuantity.appendChild(buttonQuantity);
+    settings.appendChild(itemQuantity);
+    deleteItem.appendChild(pDelete);
+    settings.appendChild(deleteItem);
+
+    return divMakeCart
 }
+
 function displayArticle(article) {
     document.querySelector("#cart__items").appendChild(article);
 
@@ -55,29 +93,36 @@ function makeArticle (item) {
     
 }
 function inserImage(item) { 
-    const div = document.createElement('div');
-    div.classList.add('cart__item__img');
+    const divImage = document.createElement('div');
+    divImage.classList.add('cart__item__img');
     const image = document.createElement('img');
     image.src = item.image
     image.alt = item.altTxt
-    div.appendChild(image);
-    return div;
+    divImage.appendChild(image);
+    return divImage;
 }
 
-function cartItemContentSettings(item) {
-    const div = document.createElement('div');
-    div.classList.add('cart__item__content__settings__quantity');
-    const quantity = document.createElement('p');
-    quantity.textContent = item.quantity;
-    div.appendChild(quantity);
-    const content = document.createElement('div');
-    content.classList.add('cart__item__content__settings__delete');
-}
+function  displayTotalQuantity(item) {
+    const totalQ = document.querySelector('#totalQuantity');
+    totalQ.textContent = item.quantity;
+ }
+
+   
+   function  displayTotalQuantity() {
+    const totalQ = document.querySelector('#totalQuantity');
+    totalQ.textContent = totalObject;
+ }
 
 
 
 
 
+     const totalObject = localStorage.quantity;
+    for (let i = 0; i < totalObject; i++) {
+        const object = localStorage.getItem(localStorage.key(i));
+        const tObject = JSON.parse(object);
+        cart.push(tObject);
+}  
 
 
 
